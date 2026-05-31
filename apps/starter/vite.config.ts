ca3718993f405b12aa9231c0931fb91438a1ddf1
vert-hydro <leonard@verticalhydration.com>
2026-05-31 14:27:30 -0500

    starter: changed tsconfigs to the monorepo ones and added ports config

--- apps/starter/vite.config.ts
+++ apps/starter/vite.config.ts
@@ -4,8 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+   server: {
+    host: "localhost",
+    port: 5180,
+    strictPort: true,
+  },
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    cloudflare({ 
+      viteEnvironment: { name: "ssr" },
+      inspectorPort: 9335, 
+    }),
     tailwindcss(),
     reactRouter(),
   ],
